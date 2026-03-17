a4735a23592a317515291839cca6b17b8c2feba3
Panav Rustagi <266935932+PanavRust@users.noreply.github.com>
2026-03-17 11:52:16 +0530

    Add plotting script for effective population size

--- SCRIPTS/plot_results.R
+++ SCRIPTS/plot_results.R
@@ -1,1 +1,20 @@
+library(ggplot2)
 
+data <- read.table("results/msmc2_output.txt", header=TRUE)
+
+colnames(data) <- c("time", "left", "right", "lambda")
+
+data$Ne <- 1 / (2 * data$lambda)
+
+ggplot(data, aes(x = left, y = Ne)) +
+  geom_line(color = "blue") +
+  scale_x_log10() +
+  scale_y_log10() +
+  theme_minimal() +
+  labs(
+    title = "Effective Population Size Over Time",
+    x = "Time",
+    y = "Ne"
+  )
+
+ggsave("plots/msmc2_plot.png")
